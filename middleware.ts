a4c08b6a9df887348fd2d799fa0e2f6564fc5041
onekyuu <me@keyu.email>
2025-05-05 04:35:48 +0800

    feat: update next-intl middleware

--- middleware.ts
+++ middleware.ts
@@ -9,11 +9,9 @@ const intlMiddleware = createMiddleware({
   localeDetection: true,
 });
 
-const publicPages = ["/login"];
-
 function detectLocaleFromAcceptLanguage(acceptLanguage: string | null): string {
-  if (!acceptLanguage) return "zh"; // 默认语言
-  const language = acceptLanguage.split(",")[0].split("-")[0]; // 提取主语言部分
+  if (!acceptLanguage) return "en";
+  const language = acceptLanguage.split(",")[0].split("-")[0];
   return SUPPORTED_LOCALES.includes(language) ? language : "en";
 }
 
@@ -24,28 +22,7 @@ export function middleware(request: NextRequest) {
     const locale = detectLocaleFromAcceptLanguage(acceptLanguage);
     return NextResponse.redirect(new URL(`/${locale}`, request.url));
   }
-  const publicPathnameRegex = RegExp(
-    `^(/(${SUPPORTED_LOCALES.join("|")}))?(${publicPages
-      .flatMap((p) => (p === "/" ? ["", "/"] : p))
-      .join("|")})/?$`,
-    "i"
-  );
-  const isPublicPage = publicPathnameRegex.test(request.nextUrl.pathname);
 
-  const localeMatch = pathname.match(/^\/(zh|en|ja)(\/|$)/);
-  const localePrefix = localeMatch?.[0] || "";
-  if (isPublicPage) {
-    return intlMiddleware(request);
-  }
-  // check access token
-  const accessToken = request.cookies.get("access_token")?.value;
-
-  if (!accessToken) {
-    const loginUrl = new URL(`${localePrefix}/login`, request.url);
-    return NextResponse.redirect(loginUrl);
-  }
-
-  // loged in
   return intlMiddleware(request);
 }
 
