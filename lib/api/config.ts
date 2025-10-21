10605d4afb7ef4272e5548974fac20c1c9d42e3e
onekyuu <me@keyu.email>
2025-10-21 18:47:07 +0800

    fix: request config

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   endpoints: {
     posts: {
       lists: "/post/lists",
